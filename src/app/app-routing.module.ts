ca265f85b501858393672e457b781e2011e069f8
Jeremylinkk <siriwut-77@hotmail.com>
2020-04-13 12:00:06 +0700

    update home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-
-
+import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   {
     path:'login',
     component:LoginComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
   }
 ];
 
